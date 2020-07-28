420fb51d78e98d7f38eb42e7d09195f4074f6c4a
saeid nasiri <name62777@gmail.com>
2020-07-28 20:28:02 +0430

    feat/Add photo play,next,previous and stop

--- view/main.py
+++ view/main.py
@@ -5,4 +5,12 @@ root = Tk()
 root.geometry('300x300')
 root.title("Player360")
 root.iconbitmap(r'../ico/360icon.ico')
+
+text = Label(root,text="Music Player")
+text.pack()
+
+img = PhotoImage(file="../ico/play.png")
+
+photo = Label(root,image=img).pack()
+
 root.mainloop()
