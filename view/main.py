f4c6c6c6717808307a30331f443981e9cfe74a76
saeid nasiri <name62777@gmail.com>
2020-07-29 18:38:17 +0430

    feat/add other button

--- view/main.py
+++ view/main.py
@@ -10,14 +10,31 @@ text = Label(root,text="Music Player")
 text.pack()
 
 playimg = PhotoImage(file="../ico/play.png")
+stopimg = PhotoImage(file="../ico/stop.png")
+nextimg = PhotoImage(file="../ico/next.png")
+previousimg = PhotoImage(file="../ico/previous.png")
 
 
 
 def play_btn():
     print("play button")
 
+def stop_btn():
+    print("stop button")
 
-    
-btn = Button(root,image=playimg,command=play_btn).pack()
+
+def next_btn():
+    print("next button")
+
+
+def previous_btn():
+    print("previous button")
+
+
+
+playtBtn = Button(root,image=playimg,command=play_btn).pack()
+stopBtn = Button(root,image=stopimg,command=stop_btn).pack()
+nextBtn = Button(root,image=nextimg,command=next_btn).pack()
+previousBtn = Button(root,image=previousimg,command=previous_btn).pack()
 
 root.mainloop()
